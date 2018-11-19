90fceec992401d985bf48a28136df665ef4d3df3
RadicalRoboticsFTC <32752482+RadicalRoboticsFTC@users.noreply.github.com>
2018-11-19 01:14:44 -0600

    Add files via upload

---
+++ AutonomousCodeM1.java
@@ -0,0 +1,284 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+@Autonomous
+public class AutonomousCodeM1 extends LinearOpMode { // defines the function
+
+    private static DcMotor FrontRight; // defines variables to be used in the code
+    private static DcMotor FrontLeft;
+    private static DcMotor BackRight;
+    private static DcMotor BackLeft;
+    private static DcMotor Arm;
+    private static DcMotor Winch;
+    private static Servo LeftMarker;
+    private static String left = "Left";
+    private static String right = "Right";
+    private static DistanceSensor Dsense;
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private TFObjectDetector tfod;
+    private int goldMineralX = -1;
+    private int silverMineral1X = -1;
+    private int silverMineral2X = -1;
+
+
+    @Override
+    public void runOpMode() { // runs code
+
+        FrontRight = (DcMotor) hardwareMap.get("FrontRight"); // this is where we assign variables to parts
+        FrontLeft = (DcMotor) hardwareMap.get("FrontLeft");
+        BackRight = (DcMotor) hardwareMap.get("BackRight");
+        BackLeft = (DcMotor) hardwareMap.get("BackLeft");
+        Arm = (DcMotor) hardwareMap.get("Arm");
+        Winch = (DcMotor) hardwareMap.get("Winch");
+        LeftMarker = (Servo) hardwareMap.get("LeftMarker");
+        Dsense = (DistanceSensor) hardwareMap.get("Dsense");
+
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Winch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Winch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+        waitForStart(); // This autonomous faces the team marker drop zone
+
+        LeftMarker.setPosition(.7);
+
+        Arm.setTargetPosition(900); //step 1: lands on field
+        Arm.setPower(.05);
+        Winch.setPower(-1);
+        while (Arm.getCurrentPosition() < Arm.getTargetPosition()) {
+
+        }
+
+        Arm.setPower(0);
+        Winch.setPower(0);
+
+        turn("Left", 100);
+
+        strafe("Right", 150);
+
+        while(goldMineralX == -1 || silverMineral1X == -1 || silverMineral2X == -1){
+            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+            if (updatedRecognitions != null) {
+                telemetry.addData("# Object Detected", updatedRecognitions.size());
+                if (updatedRecognitions.size() == 3) {
+                    goldMineralX = -1;
+                    silverMineral1X = -1;
+                    silverMineral2X = -1;
+                    for (Recognition recognition : updatedRecognitions) {
+                        if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                            goldMineralX = (int) recognition.getLeft();
+                        } else if (silverMineral1X == -1) {
+                            silverMineral1X = (int) recognition.getLeft();
+                        } else {
+                            silverMineral2X = (int) recognition.getLeft();
+                        }
+                    }
+                    if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                        if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                            telemetry.addData("Gold Mineral Position", "Left");
+                        } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                            telemetry.addData("Gold Mineral Position", "Right");
+                        } else {
+                            telemetry.addData("Gold Mineral Position", "Center");
+                        }
+                    }
+                }
+                telemetry.update();
+            }
+        }
+
+        straight(170);
+
+        if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) { //Left
+
+            turn("Left", -2100);
+
+            straight(2650);
+
+            turn("right", -1800);
+
+            straight(-1500);
+
+            LeftMarker.setPosition(1);
+
+            backwards(5060);
+
+        } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) { //Right
+
+            turn("Left", -1400);
+
+            straight(3250);
+
+            turn("left", -400);
+
+            straight(100);
+
+            backwards(-200);
+
+            turn("Left", -1895);
+
+            straight(4665);
+
+        } else { //Middle
+
+            turn("Left", -1800);
+
+            straight(2950);
+
+            LeftMarker.setPosition(1);
+
+            backwards(2650);
+
+            turn("Left", 955);
+
+            straight(7515);
+
+        }
+    }
+
+    private static void turn(String direction, int TargetPosition){
+        if(direction.equals(left)){
+            FrontLeft.setTargetPosition(TargetPosition);
+            FrontLeft.setPower(-1);
+            FrontRight.setPower(1);
+            BackLeft.setPower(-1);
+            BackRight.setPower(1);
+            while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+                /*if(Dsense < 30){
+                    FrontLeft.setPower(0);
+                    FrontRight.setPower(0);
+                    BackLeft.setPower(0);
+                    BackRight.setPower(0);
+                }*/
+            }
+
+            FrontLeft.setPower(0);
+            FrontRight.setPower(0);
+            BackLeft.setPower(0);
+            BackRight.setPower(0);
+        }
+        if(direction.equals(right)){
+            FrontLeft.setTargetPosition(TargetPosition);
+            FrontLeft.setPower(1);
+            FrontRight.setPower(-1);
+            BackLeft.setPower(1);
+            BackRight.setPower(-1);
+            while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+                /*if(Dsense < 30){
+                    FrontLeft.setPower(0);
+                    FrontRight.setPower(0);
+                    BackLeft.setPower(0);
+                    BackRight.setPower(0);
+                }*/
+            }
+
+            FrontLeft.setPower(0);
+            FrontRight.setPower(0);
+            BackLeft.setPower(0);
+            BackRight.setPower(0);
+        }
+    }
+
+    private static void straight(int TargetPosition){
+        FrontLeft.setTargetPosition(TargetPosition);
+        FrontLeft.setPower(1);
+        FrontRight.setPower(.85);
+        BackLeft.setPower(1);
+        BackRight.setPower(.85);
+        while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+                /*if(Dsense < 30){
+                    FrontLeft.setPower(0);
+                    FrontRight.setPower(0);
+                    BackLeft.setPower(0);
+                    BackRight.setPower(0);
+                }*/
+        }
+
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    private static void backwards(int TargetPosition){
+        FrontLeft.setTargetPosition(TargetPosition); //step 7: move towards crator and parks
+        FrontLeft.setPower(-1);
+        FrontRight.setPower(-.85);
+        BackLeft.setPower(-1);
+        BackRight.setPower(-.85);
+        while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+                /*if(Dsense < 30){
+                    FrontLeft.setPower(0);
+                    FrontRight.setPower(0);
+                    BackLeft.setPower(0);
+                    BackRight.setPower(0);
+                }*/
+        }
+
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+
+    private static void strafe(String direction, int TargetPosition){
+        if(direction.equals(left)){
+            FrontLeft.setTargetPosition(TargetPosition);
+            FrontLeft.setPower(-1);
+            FrontRight.setPower(.85);
+            BackLeft.setPower(1);
+            BackRight.setPower(-.85);
+            while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+                /*if(Dsense < 30){
+                    FrontLeft.setPower(0);
+                    FrontRight.setPower(0);
+                    BackLeft.setPower(0);
+                    BackRight.setPower(0);
+                }*/
+            }
+
+            FrontLeft.setPower(0);
+            FrontRight.setPower(0);
+            BackLeft.setPower(0);
+            BackRight.setPower(0);
+        }
+        if(direction.equals(right)){
+            FrontLeft.setTargetPosition(TargetPosition);
+            FrontLeft.setPower(1);
+            FrontRight.setPower(-.85);
+            BackLeft.setPower(-1);
+            BackRight.setPower(-.85);
+            while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+                /*if(Dsense < 30){
+                    FrontLeft.setPower(0);
+                    FrontRight.setPower(0);
+                    BackLeft.setPower(0);
+                    BackRight.setPower(0);
+                }*/
+            }
+
+            FrontLeft.setPower(0);
+            FrontRight.setPower(0);
+            BackLeft.setPower(0);
+            BackRight.setPower(0);
+        }
+    }
+}
+
---
+++ DriverControl.java
@@ -0,0 +1,165 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode; // this is where we import tools from other code
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServoImplEx;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+
+@TeleOp(name="Driver Control", group="Iterative Opmode")
+public class DriverControl extends OpMode { // this is where we start the function
+    private DcMotor FrontRight; // this is where we define the variables
+    private DcMotor FrontLeft;
+    private DcMotor BackRight;
+    private DcMotor BackLeft;
+    private DcMotor Arm;
+    private DcMotor Winch;
+    private static final String LABEL_GOLD_MINERAL = "Gold Mineral";
+    private TFObjectDetector tfod;
+    //private ColorSensor colorSensor;
+
+    double fr; // defining variables part 2
+    double fl;
+    double br;
+    double bl;
+    double cl;
+    double power;
+
+
+
+    @Override
+    public void init() {
+        FrontRight = (DcMotor) hardwareMap.get("FrontRight"); // this is where we assign parts to variables
+        FrontLeft = (DcMotor) hardwareMap.get("FrontLeft");
+        BackRight = (DcMotor) hardwareMap.get("BackRight");
+        BackLeft = (DcMotor) hardwareMap.get("BackLeft");
+        Arm = (DcMotor) hardwareMap.get("Arm");
+        Winch = (DcMotor) hardwareMap.get("Winch");
+        //colorSensor = (ColorSensor) hardwareMap.get("Pha");
+
+        FrontRight.setDirection(DcMotor.Direction.REVERSE); // this is where we set modes our code can execute in
+        //FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackRight.setDirection(DcMotor.Direction.REVERSE);
+        //BackLeft.setDirection(DcMotor.Direction.REVERSE);
+        Winch.setDirection(DcMotor.Direction.REVERSE);
+
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Winch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Winch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    @Override
+    public void init_loop() { // a bunch of logic when the code should execute
+    }
+
+    @Override
+    public void start() { // a bunch of logic when the code should execute
+    }
+
+
+    @Override
+    public void loop(){ // a bunch of logic when the code should execute
+        Arm.setTargetPosition(560); // sets the max position a motor can turn
+        telemetry.addData("FrontLeft Encoder", FrontLeft.getCurrentPosition()); // assigns encoders to parts
+        telemetry.addData("Arm Encoder", Arm.getCurrentPosition());
+        telemetry.addData("Winch Encoder", Winch.getCurrentPosition());
+
+        if (Arm.getCurrentPosition() < Arm.getTargetPosition() && gamepad1.dpad_up) { // checks if where motor is where it needs to be
+            Arm.setPower(.2); // sets power the motor should turn
+            Winch.setPower(1);
+        } else {
+            Arm.setPower(0); // sets power the motor should turn
+            Winch.setPower(0);
+        }
+        Winch.setTargetPosition(-560);
+        if (Winch.getCurrentPosition() < Winch.getTargetPosition() && gamepad1.dpad_down) { // checks if where motor is where it needs to be
+            Arm.setPower(-.2); // sets power the motor should turn
+            Winch.setPower(-1);
+        } else {
+            Arm.setPower(0); // sets power the motor should turn
+            Winch.setPower(0);
+        }
+
+        if (gamepad1.x) { // links controllers to commands the code should execute
+        } else if(gamepad1.y){
+        }
+
+        if (gamepad1.dpad_right) { // links controllers to commands the code should execute
+        } else if (gamepad1.dpad_left) {
+        } else if (gamepad1.dpad_down) {
+        }
+
+        if (gamepad1.right_bumper) { // links controllers to commands the code should execute
+        } else {
+        }
+
+        if (gamepad1.left_bumper) { // links controllers to commands the code should execute
+            power = .25;
+        } else {
+            power = 1;
+        }
+
+        if (gamepad1.dpad_left) { // links controllers to commands the code should execute
+        }
+
+        double gamepad1LeftY = gamepad1.left_stick_y * power; // assigns variables to inputs
+        double gamepad1LeftX = gamepad1.left_stick_x * power;
+        double gamepad1RightX = gamepad1.right_stick_x * power;
+
+        double fl = gamepad1LeftY + gamepad1LeftX + gamepad1RightX; // controller logic nightmare scary
+        double fr = gamepad1LeftY - gamepad1LeftX - gamepad1RightX; //back left
+        double br = gamepad1LeftY - gamepad1LeftX + gamepad1RightX; //back right
+        double bl = gamepad1LeftY + gamepad1LeftX - gamepad1RightX;
+
+        FrontRight.setPower(fl); // assigns power when inputs are triggered
+        FrontLeft.setPower(fr);
+        BackRight.setPower(br);
+        BackLeft.setPower(bl);
+
+        List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+        if (updatedRecognitions != null) {
+            telemetry.addData("# Object Detected", updatedRecognitions.size());
+            if (updatedRecognitions.size() == 3) {
+                int goldMineralX = -1;
+                int silverMineral1X = -1;
+                int silverMineral2X = -1;
+                for (Recognition recognition : updatedRecognitions) {
+                    if (recognition.getLabel().equals(LABEL_GOLD_MINERAL)) {
+                        goldMineralX = (int) recognition.getLeft();
+                    } else if (silverMineral1X == -1) {
+                        silverMineral1X = (int) recognition.getLeft();
+                    } else {
+                        silverMineral2X = (int) recognition.getLeft();
+                    }
+                }
+                if (goldMineralX != -1 && silverMineral1X != -1 && silverMineral2X != -1) {
+                    if (goldMineralX < silverMineral1X && goldMineralX < silverMineral2X) {
+                        telemetry.addData("Gold Mineral Position", "Left");
+                    } else if (goldMineralX > silverMineral1X && goldMineralX > silverMineral2X) {
+                        telemetry.addData("Gold Mineral Position", "Right");
+                    } else {
+                        telemetry.addData("Gold Mineral Position", "Center");
+                    }
+                }
+            }
+            telemetry.update();
+        }
+
+    }
+}

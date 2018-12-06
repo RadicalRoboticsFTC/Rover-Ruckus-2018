d7b2fa5a7534443a621a1993158f78963d9ea3eb
RadicalRoboticsFTC <32752482+RadicalRoboticsFTC@users.noreply.github.com>
2018-12-05 22:50:40 -0600

    Add files via upload

---
+++ EncoderResetTest.java
@@ -0,0 +1,73 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Autonomous
+public class EncoderResetTest extends LinearOpMode { // defines the function
+
+    private DcMotor FrontRight; // defines variables to be used in the code
+    private DcMotor FrontLeft;
+    private DcMotor BackRight;
+    private DcMotor BackLeft;
+    private DcMotor Arm;
+    private DcMotor Winch;
+    private Servo LeftMarker;
+
+    @Override
+    public void runOpMode() { // runs code
+
+        FrontRight = (DcMotor) hardwareMap.get("FrontRight"); // this is where we assign variables to parts
+        FrontLeft = (DcMotor) hardwareMap.get("FrontLeft");
+        BackRight = (DcMotor) hardwareMap.get("BackRight");
+        BackLeft = (DcMotor) hardwareMap.get("BackLeft");
+        Arm = (DcMotor) hardwareMap.get("Arm");
+        Winch = (DcMotor) hardwareMap.get("Winch");
+        LeftMarker = (Servo) hardwareMap.get("LeftMarker");
+
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Winch.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Arm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Winch.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        FrontLeft.setDirection(DcMotor.Direction.REVERSE);
+        BackLeft.setDirection(DcMotor.Direction.REVERSE);
+
+
+        waitForStart();
+        FrontLeft.setTargetPosition(100);
+        FrontLeft.setPower(1);
+        FrontRight.setPower(1);
+        BackLeft.setPower(1);
+        BackRight.setPower(1);
+        while (FrontLeft.getCurrentPosition() < FrontLeft.getTargetPosition()) {
+
+        }
+
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackLeft.setPower(0);
+
+        FrontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        FrontLeft.setTargetPosition(100);
+        FrontLeft.setPower(1);
+        FrontRight.setPower(1);
+        BackLeft.setPower(1);
+        BackRight.setPower(1);
+        while (FrontLeft.getCurrentPosition() > FrontLeft.getTargetPosition()) {
+
+        }
+
+        FrontLeft.setPower(0);
+        FrontRight.setPower(0);
+        BackLeft.setPower(0);
+        BackRight.setPower(0);
+    }
+}
